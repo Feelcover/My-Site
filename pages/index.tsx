ed1c825904fb1eccd71cd7adb17edda29519de2d
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-10-18 15:22:03 +0300

    добавить Head и метатеги, изменить лого и title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
+import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Andrey Chebanov</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type='image/svg' sizes='32x32' href="/images/logo.svg" />
+      </Head>
      <div className='backgroundAnimation'>
       <div id='ballOne'></div>
       <div id='ballTwo'></div>
