426a434391e8db0662efccf6cbe33dfd040c510b
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-10-18 15:16:34 +0300

    запустить анимацию бэкграунда

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,11 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
-     
+     <div className='backgroundAnimation'>
+      <div id='ballOne'></div>
+      <div id='ballTwo'></div>
+      <div id='ballThree'></div>
+     </div>
     </>
   )
 }
