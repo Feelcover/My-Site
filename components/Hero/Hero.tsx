80955631570b683247fabf70234a30b7959fa770
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-10-20 15:47:40 +0300

    часть структуры jsx для Hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,15 +1,37 @@
+import { useMediaQuery } from "@/hooks";
 import React from "react";
+import { Link } from "react-scroll";
 import styles from "./Hero.module.scss";
 
 const Hero = () => {
+  const isMobile = useMediaQuery(800)
   return (
     <section className={styles.hero}>
       <div className="subContainer">
         <div className={styles.hero__inner}>
           <h1 className={styles.hero__title}>Web Development</h1>
           <p className={styles.hero__description}>
-            Вы работаете над чем-то великим? Я с удовольствием помогу вам в этом! Напишите мне письмо и мы начнем проект прямо сейчас!
+            Вы работаете над чем-то великим? Я с удовольствием помогу вам в
+            этом! Напишите мне письмо и мы начнем проект прямо сейчас!
           </p>
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={30}
+            duration={500}
+            className={styles.hero__btn}
+          >
+            Связаться со разработчиком
+          </Link>
+          {!isMobile && <Link
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={30}
+            duration={500}
+            className={styles.hero__arrow}
+          />}
         </div>
       </div>
     </section>
