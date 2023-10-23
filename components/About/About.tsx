a106f9c1362b78ee66f9fa3fd6e7a6aaca811446
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-10-23 14:55:19 +0300

    сверстать компонент MainTitle + адаптив

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -5,7 +5,9 @@ import styles from "./About.module.scss";
 const About = () => {
   return (
     <section id="about">
-
+        <div className='container'>
+            <div className='subContainer'></div>
+        </div>
     </section>
   )
 }
