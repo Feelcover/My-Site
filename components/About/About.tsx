26d933d3d856a66d1f572a5420350900ba213c48
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-10-24 00:38:38 +0300

    добавить блоки SocialBar и Arrow, сделать адаптив

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,19 +1,55 @@
-import React from 'react'
-import MainTitle from '../MainTitile/MainTitle';
+import { useMediaQuery } from "@/hooks";
+import React from "react";
+import Arrow from "../Arrow/Arrow";
+import MainTitle from "../MainTitile/MainTitle";
+import SocialBar from "../SocialBar/SocialBar";
 import styles from "./About.module.scss";
 
-
 const About = () => {
+  const isMobile1080 = useMediaQuery(1080);
+
   return (
-    <section id="about">
-        <div className='container'>
-          <MainTitle text='Обо мне'/>
-            <div className='subContainer'>
-
-            </div>
+    <section id="about" className={styles.about}>
+      <div className="container">
+        <MainTitle text="Обо мне" />
         </div>
+        <div className={`'subContainer' ${styles.about__sub_container}`}>
+          <img
+            className={styles.about__img}
+            src="/images/about.jpg"
+            alt="avatar"
+          />
+          <div className={styles.about__inner}>
+            <h3 className={styles.about__title}>
+              Андрей Чебанов
+              <span className={styles.about__title__border}>
+                <span className={styles.about__title__border__arrow}>
+                  <Arrow />
+                </span>
+                <span className={styles.about__title__border__line} />
+              </span>
+            </h3>
+            <div className={styles.about__text}>
+              <p>
+                Завершил обучение в Яндекс.Практикуме, разрабатывал проекты под
+                руководством менторов и регулярными код-ревью. Так же в копилке
+                есть и собственные проекты.
+              </p>
+              <p>
+                Стэк: JavaScript/TypeScript (ES6+), React, Next, Express/Nest.
+              </p>
+              <p>В данный момент нахожусь в активном поиске работы.</p>
+            </div>
+            <div className={styles.about__social}>
+            {!isMobile1080 && <SocialBar />}
+            </div>
+          </div>
+        </div>
+        <div className="sub-container">
+                {isMobile1080 && <SocialBar />}
+            </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
---
+++ components/SocialBar/SocialBar.tsx
@@ -0,0 +1,34 @@
+import styles from './SocialBar.module.scss';
+
+const SocialBar = () => {
+    return (
+        <ul className={styles.social__bar}>
+            <li className={styles.social__bar__item}>
+                <a href="https://t.me/Feelcover" className={styles.social__bar__item__link}>
+                    <span className={`${styles.social__bar__item__link__icon} ${styles.social__bar__item__link__icon__tg}`}>
+                        <img className={styles.social__bar__item__link__img} src="/images/telegram.svg" alt="Telegram" />
+                    </span>
+                    <span className={styles.social__bar__item__link__text}>Telegram</span>
+                </a>
+            </li>
+            <li className={styles.social__bar__item}>
+                <a href="https://vk.com/begi_bratishka" className={styles.social__bar__item__link}>
+                    <span className={`${styles.social__bar__item__link__icon} ${styles.social__bar__item__link__icon__vk}`}>
+                        <img className={styles.social__bar__item__link__img} src="/images/vk.svg" alt="ВКонтакте" />
+                    </span>
+                    <span className={styles.social__bar__item__link__text}>ВКонтакте</span>
+                </a>
+            </li>
+            <li className={styles.social__bar__item}>
+                <a href="#" className={styles.social__bar__item__link}>
+                    <span className={`${styles.social__bar__item__link__icon} ${styles.social__bar__item__link__icon__gh}`}>
+                        <img className={styles.social__bar__item__link__img} src="/images/github.svg" alt="GitHub" />
+                    </span>
+                    <span className={styles.social__bar__item__link__text}>Github</span>
+                </a>
+            </li>
+        </ul>
+    );
+}
+
+export default SocialBar;
