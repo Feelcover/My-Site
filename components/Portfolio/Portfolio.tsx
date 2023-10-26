fcb85edd9398c0a57981369442c75c948167679b
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-10-26 18:41:40 +0300

    вёрстка компонента Portfolio и PortfolioTab

--- components/Portfolio/Portfolio.tsx
+++ components/Portfolio/Portfolio.tsx
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import MainTitle from "../MainTitile/MainTitle";
-import styles from "./Portfolio.module.scss"
+import styles from "./Portfolio.module.scss";
+import PortfolioTab from "./PortfolioTab";
 
 const Portfolio = () => {
+  const [tab, setTab] = useState({
+    tabAll: true,
+    tabStatic: false,
+    tabReact: false,
+  });
+  const [hiddenAllItems, setHiddenAllItems] = useState(true);
+  const toggleHiddenAllItems = () => {
+    setHiddenAllItems(!hiddenAllItems);
+  };
+  const showTabAll = () => {
+    setTab({ tabAll: true, tabStatic: false, tabReact: false });
+  };
+  const showTabStatic = () => {
+    setTab({ tabAll: false, tabStatic: true, tabReact: false });
+  };
+  const showTabReact = () => {
+    setTab({ tabAll: false, tabStatic: false, tabReact: true });
+  };
+
+  const tabController = [
+    { id: 1, title: "Все проекты", isActive: tab.tabAll, handler: showTabAll },
+    {
+      id: 2,
+      title: "Статические сайты",
+      isActive: tab.tabStatic,
+      handler: showTabStatic,
+    },
+    { id: 3, title: "React", isActive: tab.tabReact, handler: showTabReact },
+  ];
   return (
     <section className={styles.portfolio} id="porfolio">
       <div className="container">
-      <MainTitle text="Портфолио" />
+        <MainTitle text="Портфолио" />
       </div>
       <div className="subContainer">
-
+        <div className={styles.portfolio__inner}>
+          <div className={styles.portfolio__inner__wrapper}>
+            {tabController.map((item) => (
+              <PortfolioTab
+                key={item.id}
+                title={item.title}
+                isActive={item.isActive}
+                handleShowTab={item.handler}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
--- components/Portfolio/PortfolioTab.tsx
+++ components/Portfolio/PortfolioTab.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { FC } from "react";
+import Arrow from "../Arrow/Arrow";
+import styles from "./Portfolio.module.scss";
 
-const PortfolioTab = () => {
-  return (
-    <div>PortfolioTab</div>
-  )
+interface IPortfolioTabControl {
+  title: string;
+  isActive: boolean;
+  handleShowTab: () => void;
 }
 
-export default PortfolioTab
+const PortfolioTab: FC<IPortfolioTabControl> = ({
+  title,
+  isActive,
+  handleShowTab,
+}) => {
+  return (
+    <li className={styles.portfolio__tabs__item}>
+      <button
+        className={styles.portfolio__tabs__item__btn}
+        onClick={handleShowTab}
+      >
+        <span
+          className={styles.portfolio__tabs__item__btn__text}
+          style={{ color: isActive ? "#F2F2F2" : "#B7BFD1" }}
+        >
+          {title}
+        </span>
+        {isActive && (
+          <span className={styles.portfolio__tabs__item__btn__arrow}>
+            <Arrow />
+          </span>
+        )}
+        <span
+          className={`
+            ${styles.portfolio__tabs__item__btn__border} ${
+            isActive ? styles.portfolio__tabs__item__btn__border_active : ""
+          }`}
+        />
+      </button>
+    </li>
+  );
+};
+
+export default PortfolioTab;
