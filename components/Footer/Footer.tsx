1f487b9b7bdc0980dcf5dc25b67331f6d15afd6d
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-11-13 14:28:49 +0300

    часть структуры футера

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.scss";
 
 const Footer = () => {
   return (
-    <footer>
-        
+    <footer className={styles.footer}>
+      <div className={`container ${styles.footer__container}`}>
+        <span className={styles.footer} />
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
